88c6aa8a400030bcfcd095ea461791725f9911c8
HieuNguyen <hieu30032000@gmail.com>
2023-09-25 15:44:24 +0700

    u

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(public PhotoService: PhotoService, public actionSheetController: ActionSheetController) {}
+
+  async ngOnInit() {
+    await this.PhotoService.loadSaved();
+  }
+
+  addPhotoGallery() {
+    this.PhotoService.addNewToGallery();
+  }
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.PhotoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+}
